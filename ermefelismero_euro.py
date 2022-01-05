5e417c00d6850e850d8fd9f8e9d73b43e88dc848
Gábor Gerse <gabor.gerse@flex.com>
2022-01-05 23:44:03 +0100

    add comments

--- ermefelismero_euro.py
+++ ermefelismero_euro.py
@@ -31,26 +31,33 @@ output = image.copy()
 
 #szurke aranyalatosra konvertalas
 #https://www.geeksforgeeks.org/python-opencv-cv2-cvtcolor-method/
+#https://docs.opencv.org/4.x/d8/d01/group__imgproc__color__conversions.html#ga397ae87e1288a81d2363b61574eb8cab
 #gepi latas problemainak megoldasa. Sz
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # cv2.imshow("Szurke kep", gray)
 
 #kontraszt javitas a fenyviszonyok kozotti kulonbsegekkel
-#clachbe belerak a kontraszt kepet a hisztorgram alkalmazasahoz
-clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+#clachbe belerak a kontraszt kepet a hisztorgram alkalmazasahoz ?????
+#https://docs.opencv.org/4.x/d6/dc7/group__imgproc__hist.html#gad689d2607b7b3889453804f414ab1018
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8)) #kontraszt koszoberteke, racs merete
 gray = clahe.apply(gray)
+# cv2.imshow("Szurke kep javitva", gray)
 
-
+#function letrehozas. Bemeno parameter az img
 def calcHistogram(img):
     #maszk letrehozas
-    m = np.zeros(img.shape[:2], dtype="uint8")
-    (w, h) = (int(img.shape[1] / 2), int(img.shape[0] / 2))
-    cv2.circle(m, (w, h), 60, 255, -1)
+    m = np.zeros(img.shape[:2], dtype="uint8") #img dimenziojanak elso ket eleme, tipus unassigned int 8 bit. 255on feluli ertekek levagasa
+    (w, h) = (int(img.shape[1] / 2), int(img.shape[0] / 2)) #masodik es elso ertek osztva kettovel
+    #https://www.geeksforgeeks.org/python-opencv-cv2-circle-method/
+    cv2.circle(m, (w, h), 60, 255, -1) #koroket rajzol a kepre. m a kep, a kor kozeppontja, 60 radius, szin, korvonal vastagsaga. -1 a kor alakja 
 
-    #calcHist-nek atad kep, szin csatornak, maszk, felbontas, tartomany
+    #calcHist-nek atad kep, szin csatornak, maszk, felbontas, tartomany teljes mivel 0,256
+    #tomkeszletnek a hisztogramjat szmaolja ki
+    #https://docs.opencv.org/4.x/d1/db7/tutorial_py_histogram_begins.html
     h = cv2.calcHist([img], [0, 1, 2], m, [8, 8, 8], [0, 256, 0, 256, 0, 256])
 
     #normalizalt lapitott histogram
+    #https://docs.opencv.org/3.4/d2/de8/group__core__array.html#ga87eef7ee3970f86906d69a92cbf064bd
     return cv2.normalize(h, h).flatten()
 
 
@@ -59,13 +66,15 @@ def calcHistFromFile(file):
     return calcHistogram(img)
 
 
-#enum
+#enum, tuple jelenti, hogy tobb ertek egy valtozoba
 class Enum(tuple): __getattr__ = tuple.index
 
 #rez, sragarez
+#material feltoltese
 Material = Enum(('Copper', 'Brass', 'Euro1', 'Euro2'))
 
-#pelda kepek helye
+#pelda kepek helye. glob minden fajlt megtalal az adott mapban a * patternel. 
+#https://docs.python.org/3/library/glob.html
 sample_images_copper = glob.glob("sample_images/copper/*")
 sample_images_brass = glob.glob("sample_images/brass/*")
 sample_images_euro1 = glob.glob("sample_images/euro1/*")
@@ -76,6 +85,7 @@ X = []
 y = []
 
 #adatok es feliratok tarolasa for cuklissal
+#sample_images elemein vegig megy es atadja a calchist-nek majd az eredmenyet xhez fuzi. y-ba pedig belerakja a marail enumot (sorszamat)
 for i in sample_images_copper:
     X.append(calcHistFromFile(i))
     y.append(Material.Copper)
@@ -89,31 +99,44 @@ for i in sample_images_euro2:
     X.append(calcHistFromFile(i))
     y.append(Material.Euro2)
 
+# print(sample_images_copper)
+# print(X)
+# print(y)
+
 #osztalyozo eredmenye
+# https://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPClassifier.html
+# lbfgs Newton modszerbol szarmazo optimalizalo.
+# nyilt forraskodu gepi tanulas modulok adat elemzeshez
 clf = MLPClassifier(solver="lbfgs")
 
 #trening es teszt adatok felosztasa
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=.2)
+#https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+#tombok felosztasa veletlenszeru test es trening adatokra
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 #trening es eredmenyek osztalyozasa
+#.fit-el illeszti az adat matrix x-et az y celokra
+#.score az atlagos pontossag a tesztadatokon
 clf.fit(X_train, y_train)
 score = int(clf.score(X_test, y_test) * 100)
 print("A vizsgalat pontossaga: ", score)
 
-#blurrel Gaussia elmosas szurke aranyalatos
-#7x7-es kernel, az automatikus erzekelshez 0
+#kep elhomalyositasa gauss szuro hasznaltaval
+#bemeno a kep, 7x7-es kernel, az automatikus erzekelshez 0. Ilyenkor a kerenlbol szamolodik a sigma x es y
+#https://docs.opencv.org/4.x/d4/d86/group__imgproc__filter.html#gaabe8c836e97159a9193fb0b11ac52cf1
 blurred = cv2.GaussianBlur(gray, (7, 7), 0)
 
 #kor vektorba taroljuk, amit eremnek gondol 3 paramaterrel. x,y,r. Kozeppont es sugar. 
-#blurred a bementi szurke kep, metodus, dp inverz ratio, min_dist tavolsag a kozeppontok kozott, param_1,param_2 canny erzekelo ertekei, min_radius, max_radius sugar meretek.
+#blurred a bementi szurke kep, metodus hough gradeint az elek gradiens infoit hasznalja, dp inverz ratio 1-nel egyforma a bementivel,de 2-nel fele akkora mar
+#min_dist tavolsag a kozeppontok kozott, param_1,param_2 canny eldetektalonak atadott ertekek, min_radius, max_radius sugar meretek.
+#https://docs.opencv.org/4.x/dd/d1a/group__imgproc__feature.html#ga47849c3be0d0406ad3ca45db65a25d2d
 circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=2.2, minDist=100,
                            param1=200, param2=100, minRadius=50, maxRadius=120)
 
 #anyag felismerese, tipus joslas majd visszadas
 def predictMaterial(roi):
     hist = calcHistogram(roi)
-    s = clf.predict([hist])
+    s = clf.predict([hist]) #elorejelzes a tobb retego perceptron osztalyozo segitsegevel
     return Material[int(s)]
 
 diameter = []
